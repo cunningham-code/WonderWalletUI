c854d334db7d3cb3fba92e771ca47014cb0f3536
willperacchio <willperacchio@users.noreply.github.com>
2018-04-07 17:37:04 -0400

    t

--- src/App.js
+++ src/App.js
@@ -1,20 +1,214 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Favorites from './favorites.js'
+import Profile from './profile.js'
+import Autocomplete from 'react-autocomplete'
+
+const favoritesArray = [
+  {
+    publicID: "0xb8f1713a4a1b4d64b66b1785108c8d2b200441b370278cd2fedb38de8e8b2f90 ",
+    color: '#8E4A49',
+  },
+  {
+    publicID: "0xb8f1713a4a1b4d64b66b1785108c8d2b200441b370278cd2fedb38de8e8b2f90 ",
+    color: '#2B59C3',
+  },
+  {
+    publicID: "0xb8f1713a4a1b4d64b66b1785108c8d2b200441b370278cd2fedb38de8e8b2f90 ",
+    color: '#533A7B',
+  },
+  {
+    publicID: "0xb8f1713a4a1b4d64b66b1785108c8d2b200441b370278cd2fedb38de8e8b2f90 ",
+    color: '#253C78',
+  },
+  {
+    publicID: "0xb8f1713a4a1b4d64b66b1785108c8d2b200441b370278cd2fedb38de8e8b2f90 ",
+    color: '#D36582',
+  },
+];
+
+const Userr = {
+  currentBalance: 6900,
+  publicKey: "01234567890123456789012345678901234567890123456789"
+}
 
 class App extends Component {
+  state = {
+    screen: 0,
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    if(this.state.screen === 0) {
+      return (
+        <div style={{marginTop: 50}}>
+            <div className="row topRow center-align">
+                <div className="wonder center-align">W&#0246;nderWallet</div>
+                <div className="tagline center-align">It is time to blockchain human again</div>
+            </div>
+
+            <div className="row bottomRow">
+              <div className="container center-align">
+                <h4>Get a Keystore</h4>
+                <form action="#">
+                  <div className="row formRow">
+                    <div className="col m3"></div>
+                    <div className="input-field col s12 m6">
+                      <i className="material-icons prefix">lock</i>
+                      <input id="password" type="password" className="validate"></input>
+                      <label for="password">Password</label>
+                    </div>
+                  </div>
+                    <button onClick={() => this.setState({screen: 2}) } className="btn waves-effect btn-black btn-bottom waves-light" >Generate My Keystore</button>
+                </form>
+
+                <div className="preloader-wrapper active hide">
+                  <div className="spinner-layer spinner-red-only">
+                    <div className="circle-clipper left">
+                      <div className="circle">
+                      </div>
+                    </div>
+                    <div className="gap-patch">
+                      <div className="circle">
+                      </div>
+                    </div>
+                      <div className="circle-clipper right">
+                        <div className="circle">
+                        </div>
+                      </div>
+                  </div>
+                </div>
+
+                <div className="container hide">
+                  <p onClick={() => this.setState({screen: 2}) } className="waves-effect btn-black btn-bottom waves-light btn-large"><i className="material-icons">file_download</i>Download My Keystore</p>
+                </div>
+                <div className="chipz">
+                  <div className="chip chip-custom active-chip">
+                    Create A Keystore
+                  </div>
+                  <div onClick={() => this.setState({screen: 1}) } className="chip chip-custom">
+                    Upload A Keystore
+                  </div>
+                </div>
+              </div>
+            </div>
+            </div>
+      );
+    } else if(this.state.screen === 1) {
+      return (
+        <div style={{marginTop: 50}}>
+        <div className="row topRow center-align">
+          <div className="wonder center-align">W&#0246;nderWallet</div>
+          <div className="tagline center-align">It is time to blockchain human again</div>
+        </div>
+        <div className="row bottomRow">
+          <div className="container center-align">
+            <h4>Upload a Keystore</h4>
+            <form action="#">
+              <div className="row formRow">
+                <div className="col m3"></div>
+                <div className="col s12 m6 file-field input-field">
+                  <div className="btn btn-black">
+                    <span>File</span>
+                    <input id="file" type="file"></input>
+                  </div>
+                  <div className="file-path-wrapper">
+                    <input id="filename" className="btn-black file-path validate" type="text" value="Upload Keystore Here"></input>
+                  </div>
+                </div>
+              </div>
+              <div className="row formRow">
+                <div className="col m3"></div>
+                <div className="input-field col s12 m6">
+                  <i className="material-icons prefix">lock_open</i>
+                  <input id="password" type="password" className="validate"></input>
+                  <label for="password">Password</label>
+                </div>
+              </div>
+                <button onClick={() => this.setState({screen: 2}) } className="btn btn-black btn-bottom waves-effect waves-light" >Submit</button>
+            </form>
+            <div className="chipz">
+              <div onClick={() => this.setState({screen: 0}) } className="chip chip-custom">
+                Create A Keystore
+              </div>
+              <div className="chip chip-custom active-chip">
+                Upload A Keystore
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <div className="row topRow center-align">
+            <div className="wonder center-align">W&#0246;nderWallet</div>
+            <div className="tagline center-align">It is time to blockchain human again</div>
+          </div>
+          <div className="scrollmenu">
+            <Favorites favoritesArray={ favoritesArray } />
+          </div>
+          <div className="row fav-row">
+            <ul className="collection with-header col s12 center-align sideBar">
+
+            </ul>
+          </div>
+          <div className="row profile-row">
+            <Profile user={ Userr } />
+          </div>
+          <div className="row form-row">
+            <div className="row col s12 m8">
+              <hr></hr>
+              <h4>Make a Payment:</h4>
+              <form className="col s12">
+                <div className="row formRow">
+                  <div className="input-field col s12 m11">
+                    <i className="material-icons prefix">face</i>
+                    <textarea id="receiver" className="materialize-textarea"></textarea>
+                    <label for="receiver">Receiver Public Key</label>
+                  </div>
+                </div>
+                <div className="row formRow">
+                  <div className="input-field col s7 m8">
+                    <i className="material-icons prefix">local_atm</i>
+                    <textarea id="amount" className="materialize-textarea"></textarea>
+                    <label for="amount">Amount</label>
+                  </div>
+                  <div className="input-field col s5 m3">
+                    <h5>= $666.69 USD</h5>
+                  </div>
+                </div>
+                <div className="row formRow">
+
+                  <div className="input-field col s12 m6">
+                  <label for="autocomplete-input">Ticker Lookup</label>
+                  <Autocomplete
+                    getItemValue={(item) => item.label}
+                    items={[
+                      { label: 'AppCoins' },
+                      { label: 'EOS' },
+                      { label: 'Loopring' },
+                      { label: 'Snovio' },
+                      { label: 'WETH' }
+                    ]}
+                    renderItem={(item, isHighlighted) =>
+                      <div style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
+                        {item.label}
+                      </div>
+                    }
+                    value={this.state.value}
+                    onChange={e => this.setState({ value: e.target.value })}
+                    onSelect={value => this.setState({ value })}
+                  />
+                  </div>
+
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      );
+    }
   }
 }
 
